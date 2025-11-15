25408bd4f1a25c8052276121345eea0c96ad4b68
exclusivityadm <exclusivity.adm@gmail.com>
2025-11-15 15:59:42 -0500

    Create page.tsx

---
+++ apps/backend/page.tsx
@@ -0,0 +1,11 @@
+import TwinsPlay from "./components/TwinsPlay";
+
+export default function BackendPage() {
+  return (
+    <main className="min-h-dvh bg-white text-black">
+      <div className="max-w-xl mx-auto">
+        <TwinsPlay />
+      </div>
+    </main>
+  );
+}
