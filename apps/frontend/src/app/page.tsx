fa0b340d7ad17bb67258ae0cefdd34f8018d2f2f
exclusivityadm <exclusivity.adm@gmail.com>
2025-12-14 11:06:59 -0500

    Update page.tsx

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { SETTINGS } from "@/lib/settings";
+import { SETTINGS } from "@/app/lib/settings";
 
 export default function Home() {
   const router = useRouter();
